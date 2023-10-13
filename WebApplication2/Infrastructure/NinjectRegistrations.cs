74223d72c751b7fa00b646be135c77aff5dfad5c
Yarosalv <yaroslavgh2016@gmail.com>
2023-10-13 16:43:56 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Infrastructure/NinjectRegistrations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+using WebApplication2.Models;
+
+namespace WebApplication2.Infrastructure
+{
+    using Ninject.Modules;
+    using Ninject.Web.Common;
+
+    public class NinjectRegistrations : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValueCalculator>().To<LinqValueCalculator>().InRequestScope();
+            Bind<IDiscountHelper>().To<DefaultDiscountHelper>()
+                                        .WithConstructorArgument("discountParam", 10M);
+            Bind<IDiscountHelper>().To<FlexibleDiscountHelper>()
+                                        .WhenInjectedInto<LinqValueCalculator>();
+        }
+    }
+
+}
---
+++ WebApplication2/Models/Discount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public interface IDiscountHelper
+    {
+        decimal ApplyDiscount(decimal totalParam);
+    }
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+        private decimal discountSize;
+        public DefaultDiscountHelper(decimal discountParam)
+        {
+            discountSize = discountParam;
+        }
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (discountSize / 100m * totalParam));
+        }
+    }
+
+
+
+}
